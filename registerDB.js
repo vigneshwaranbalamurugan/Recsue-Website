d61d10eef6f0bc008a7a773fe046d36688147dcb
vigneshwaranbalamurugan <125852792+vigneshwaranbalamurugan@users.noreply.github.com>
2023-10-06 20:51:45 +0530

    Add files via upload

---
+++ registerDB.js
@@ -0,0 +1,46 @@
+ const firebaseConfig = {
+    apiKey: "AIzaSyDhk_8bhmMUo2pclKIksTpbVgftyB_IFRY",
+    authDomain: "register1-45164.firebaseapp.com",
+    databaseURL: "https://register1-45164-default-rtdb.firebaseio.com",
+    projectId: "register1-45164",
+    storageBucket: "register1-45164.appspot.com",
+    messagingSenderId: "614555310804",
+    appId: "1:614555310804:web:47e87fd212f77b6b0db941"
+  };
+firebase.initializeApp(firebaseConfig);
+var registerDB=firebase.database().ref('Registeration');
+document.getElementById("Registeration").addEventListener('submit',submitForm);
+function submitForm(e){
+	e.preventDefault();
+	var fname=getElementVal('firstName');
+	var lname=getElementVal('lastName');
+	var email=getElementVal('email');
+	var Password=getElementVal('password');
+	var number=getElementVal('number');
+	 var ele = document.getElementsByName('gender');
+    var gender;
+            for (i = 0; i < ele.length; i++) {
+                if (ele[i].checked)
+                    gender=ele[i].value;
+            }
+        
+	console.log(fname,lname,gender);
+	saveMessage(fname,lname,email,Password,number,gender);
+	
+}
+const saveMessage=(firstName,lastName,email,Password,number,Gender)=>{
+	var newRegister=registerDB.push();
+	newRegister.set({
+		FirstName:firstName,
+		LastName:lastName,
+		Email:email,
+		Password:Password,
+		MobileNumber:number,
+		Gender:Gender,
+		Date:Date(),
+		
+	})
+};
+const getElementVal = (id)=>{
+	return document.getElementById(id).value;
+};
